76a1010e7b3b573291390f558fd7833fd9338213
Tim Kamanin <tim@timonweb.com>
2020-02-03 18:36:54 +0100

    Releases 0.3.0

--- non_admin_draftail/widgets.py
+++ non_admin_draftail/widgets.py
@@ -1,14 +1,16 @@
 from django import forms
 from django.forms import Media
+from django.utils.functional import cached_property
 from wagtail.admin.rich_text import DraftailRichTextArea
+from wagtail.admin.staticfiles import versioned_static
 
 
 class NonAdminDraftailRichTextArea(forms.TextInput, DraftailRichTextArea):
     template_name = 'non_admin_draftail/widgets/non_admin_draftail_rich_text_area.html'
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._media = Media(js=[
+    @cached_property
+    def media(self):
+        media = Media(js=[
             'non_admin_draftail/draftail/draftail.js',
         ], css={
             'all': [
@@ -20,6 +22,11 @@ class NonAdminDraftailRichTextArea(forms.TextInput, DraftailRichTextArea):
                 'non_admin_draftail/fonts/wagtail-font.css'
             ]
         })
+
+        for plugin in self.plugins:
+            media += plugin.media
+
+        return media
 
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
