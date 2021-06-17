34967c22bd1f3072d41b8e16d6207e536173840a
Adam Johnson <me@adamj.eu>
2021-06-17 16:01:22 +0100

    Avoid RemovedInDjango41Warning on Django 3.2
    ```
      /.../django/apps/registry.py:91: RemovedInDjango41Warning: 'non_admin_draftail' defines default_app_config = 'non_admin_draftail.apps.NonAdminDraftailConfig'. Django now detects this configuration automatically. You can remove default_app_config.
        app_config = AppConfig.create(entry)

--- non_admin_draftail/__init__.py
+++ non_admin_draftail/__init__.py
@@ -1,1 +1,5 @@
-default_app_config = "non_admin_draftail.apps.NonAdminDraftailConfig"
+import django
+
+
+if django.VERSION < (3, 2):
+      default_app_config = "non_admin_draftail.apps.NonAdminDraftailConfig"
